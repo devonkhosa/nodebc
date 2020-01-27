24eca7b897bda640a9d55f0be6cd4646ff0f697c
Devon Khosa <dalvinderkhosa@gmail.com>
2020-01-27 10:45:58 -0800

    Added ex_28

--- js/ex_28/index28.js
+++ js/ex_28/index28.js
@@ -1,2 +1,21 @@
-let number = 9
-let multi1 = `${number} times 1`
+const num = 9;
+let template1 = ` times 1 equals ${num*1}`;
+let template2 = ` times 2 equals ${num*2}`;
+let template3 = ` times 3 equals ${num*3}`;
+let template4 = ` times 4 equals ${num*4}`;
+let template5 = ` times 5 equals ${num*5}`;
+let template6 = ` times 6 equals ${num*6}`;
+let template7 = ` times 7 equals ${num*7}`;
+let template8 = ` times 8 equals ${num*8}`;
+let template9 = ` times 9 equals ${num*9}`;
+let template10 = ` times 10 equals ${num*10}`;
+console.log(num + template1);
+console.log(num + template2);
+console.log(num + template3);
+console.log(num + template4);
+console.log(num + template5);
+console.log(num + template6);
+console.log(num + template7);
+console.log(num + template8);
+console.log(num + template9);
+console.log(num + template10);
