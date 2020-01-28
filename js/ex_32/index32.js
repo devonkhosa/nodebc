daf87459f953adb55c993cec79842a0d0307b10a
devonkhosa <dalvinderkhosa@gmail.com>
2020-01-27 20:17:04 -0800

    Added ex_32

---
+++ js/ex_32/index32.js
@@ -0,0 +1,8 @@
+let height = 10;
+let base = 4;
+let perimeter = 2*(4+10);
+let area = 4*10;
+let templatePerimeter = `The perimeter of a rectangle with a base of 4 and height of 10 is ${perimeter}`;
+let templateArea = `The area of a rectangle with a base of 4 and height of 10 is ${area}`;
+console.log(templatePerimeter);
+console.log(templateArea);
