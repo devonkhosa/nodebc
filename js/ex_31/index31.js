5a85fd2843825e6438dec0fee77cd66bdcf3caf1
devonkhosa <dalvinderkhosa@gmail.com>
2020-01-27 20:04:36 -0800

    Added ex_31

---
+++ js/ex_31/index31.js
@@ -0,0 +1,6 @@
+let side = 10;
+let side2 = 20;
+let side3 = 5;
+let perimeter = 10 + 20 + 5;
+let template = `The numbers 10, 20, and 5, make up the sides of a triangle, with a total perimeter of ${perimeter}`;
+console.log(template);
