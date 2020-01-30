72c8707890f79a47881d6bcff8cb5721f9c9d24d
Devon Khosa <devonkhosa@Devons-MacBook-Pro.local>
2020-01-30 15:01:21 -0800

    Added ex_34

--- js/ex_34/index34.js
+++ js/ex_34/index34.js
@@ -1,4 +1,4 @@
-let firstNumber = 20;
+/*let firstNumber = 20;
 let secondNumber = 20;
 let thirdNumber = 30;
 console.log(firstNumber == secondNumber); 
@@ -7,9 +7,11 @@ console.log(firstNumber == thirdNumber);
 let ten = 10;
 console.log(ten == '10');
 
-//ex_34 begins below
+ex_34 begins below*/
 
 let income = 1000;
 let revenue = 600;
 let taxes = 500;
-let thisMonth = 
+console.log(`Income Objective: ${income >=800}`);
+console.log(`Taxes Objective: ${taxes <=400}`);
+console.log(`Bonus Objective: ${revenue ==600}`);
