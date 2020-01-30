900cf1399bf075e1fde2a0c857fe4b93b2d79374
Devon Khosa <devonkhosa@Devons-MacBook-Pro.local>
2020-01-29 18:05:31 -0800

    Work in progress

---
+++ js/ex_34/index34.js
@@ -0,0 +1,15 @@
+let firstNumber = 20;
+let secondNumber = 20;
+let thirdNumber = 30;
+console.log(firstNumber == secondNumber); 
+console.log(firstNumber == thirdNumber);  
+
+let ten = 10;
+console.log(ten == '10');
+
+//ex_34 begins below
+
+let income = 1000;
+let revenue = 600;
+let taxes = 500;
+let thisMonth = 
