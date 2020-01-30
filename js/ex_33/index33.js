e091d69939fe0672827193e081ef7f93df1fe915
Devon Khosa <devonkhosa@Devons-MacBook-Pro.local>
2020-01-29 17:25:11 -0800

    Made changes to ex_33

--- js/ex_33/index33.js
+++ js/ex_33/index33.js
@@ -1,7 +1,7 @@
 let userCount = 100;
 userCount +=5;
 console.log(`User count: ${userCount}`);
-userCount -=5;
+userCount -=3;
 console.log(`User count: ${userCount}`);
 userCount *=2;
 console.log(`User count: ${userCount}`);
