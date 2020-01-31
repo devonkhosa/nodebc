fca8c51c989febe84d886d82ae91995c4b3e6a4b
Devon Khosa <devonkhosa@Devons-MacBook-Pro.local>
2020-01-30 21:30:45 -0800

    Added ex_35

---
+++ js/ex_35/index35.js
@@ -0,0 +1,5 @@
+let clientId = 143245;
+let inputClientId = '143245';
+console.log(`Does the client ID match? ${clientId == inputClientId}`);
+let template = `${typeof clientId == typeof inputClientId}`
+console.log(`Does the client id match? ${clientId == inputClientId} and is it the right type? ${template}`);
