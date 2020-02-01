e7422bd0b60b1f7e331b91cc46bb9fade5118866
Devon Khosa <devonkhosa@Devons-MacBook-Pro.local>
2020-01-31 18:51:40 -0800

    Made changes to ex_39

--- js/ex_39/index39.js
+++ js/ex_39/index39.js
@@ -1,5 +1,7 @@
 let firstName = 'Devon';
 let lastName = 'Khosa';
+let CharsDiff = firstName.length - lastName.length;
 console.log(`My name is ${firstName}, and it is ${firstName.length} characters long!`)
 console.log(`My last name is ${lastName}, and it is ${lastName.length} characters long!`)
 console.log(`My first and last name are the same length: ${firstName.length == lastName.length}`);
+console.log(`The difference between my first name and last name is ${CharsDiff}`);
