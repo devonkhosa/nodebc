9a90cf3282b67cca4b0e680c69b6efd6b3f06a46
Devon Khosa <devonkhosa@Devons-MacBook-Pro.local>
2020-02-20 21:03:24 -0800

    Added ex_71

---
+++ js/ex_71/index71.js
@@ -0,0 +1,8 @@
+const number = 9;
+let multiple = 0;
+let result = 0;
+while (multiple<10) {
+    multiple++
+    result = number*multiple;
+    console.log(`${number} times ${multiple} is ${result}`)
+}
