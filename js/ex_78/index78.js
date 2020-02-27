b0b96fdebc7894ac037f43b41cb2f0263ad37a97
devonkhosa <dalvinderkhosa@gmail.com>
2020-02-26 17:15:20 -0800

    Added ex_76 - ex_80

---
+++ js/ex_78/index78.js
@@ -0,0 +1,7 @@
+let n = 0;
+
+do {
+    if(n %2==0)
+    console.log(n);
+    n++;
+}   while (n<=100)
---
+++ js/ex_80/index80.js
@@ -0,0 +1,7 @@
+let n = 0;
+
+do {
+    if (n <=1000, n >=0)
+    console.log(n);
+    n+=2;
+}   while()
